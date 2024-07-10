f9d57f000a6b5aa445d3c225845edab0da41d212
Nazar <sydyahanazar777@gmail.com>
2024-07-10 14:38:27 +0200

    feat: add app reload button

--- navigation/Navigator.tsx
+++ navigation/Navigator.tsx
@@ -1,16 +1,21 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Restart } from "fiction-expo-restart";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
-
-import {SearchScreen} from '../screens/Search';
+import { SearchScreen } from '../screens/Search';
 import { TabNavigatorParamsList } from '../types/navigation';
 
 const TabNavigator = createBottomTabNavigator<TabNavigatorParamsList>();
 
+const ReloadButton = () => (
+    <AntDesign.Button name="reload1" size={24} color="black" backgroundColor="transparent" onPress={Restart} />
+);
+
 export const Navigator = () => {
   return (
     <NavigationContainer>
-      <TabNavigator.Navigator>
+      <TabNavigator.Navigator screenOptions={{ headerRight: ReloadButton }}>
         <TabNavigator.Screen name="Search" component={SearchScreen} />
       </TabNavigator.Navigator>
     </NavigationContainer>
